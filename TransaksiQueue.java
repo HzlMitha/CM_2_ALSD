f08fe4b8ed578165d1b05bfbb8ebc82fc23c3b98
Adelia Rizki <adeliarizkiekaputr@gmail.com>
2025-06-06 17:31:54 +0700

    konstruktor dan method TransaksiQueue

--- TransaksiQueue.java
+++ TransaksiQueue.java
@@ -5,5 +5,19 @@ public class TransaksiQueue {
     int size;
     int max;
 
+    public TransaksiQueue(int n) {
+        max = n;
+        data = new TransaksiLayanan[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == max;
+    }
     
 }
